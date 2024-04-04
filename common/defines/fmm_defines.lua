34d39d1461e707fa553b3c744d7460fd6bca2a03
JustFalcxn <JPGarrett22@gmail.com>
2024-04-04 14:27:56 -0500

    Rework of Commits & Unused Marines Modifier Added

---
+++ common/defines/fmm_defines.lua
@@ -0,0 +1,17 @@
+-- Condottieri defines - need playtesting!
+
+NDefines.NCountry.MAXIMUM_CONDOTTIERI = 40
+NDefines.NAI.DIPLOMATIC_ACTION_OFFER_CONDOTTIERI_ONLY_MILITARY_RULERS = 0
+
+
+-- Heir Defines
+
+NDefines.NCountry.ABDICATE_AGE_THRESHOLD = 50
+NDefines.NCountry.ABDICATE_RULING_LENGTH_THRESHOLD = 10
+NDefines.NCountry.ABDICATE_PRESTIGE_HIT = -25
+NDefines.NCountry.DISINHERIT_PRESTIGE_HIT = -15
+NDefines.NCountry.DISINHERIT_PRESTIGE_THRESHOLD = -15
+NDefines.NCountry.NEW_HEIR_LEGITIMACY_THRESHOLD = 80
+
+-- Experimental Province Cost
+NDefines.NCountry.PS_DEMAND_NON_WARGOAL_PROVINCE = 35
